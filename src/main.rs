8d34c1859e8cc9421d8344158507249940ebd320
vladIev <vlad.levotskiy@gmail.com>
2025-04-29 03:37:35 +0300

    Updates processor

--- src/main.rs
+++ src/main.rs
@@ -3,107 +3,27 @@ mod depth_updates;
 mod updates_processor;
 mod updates_provider;
 
-use book_order::{BookOrder, DepthUpdateError};
 use depth_updates::DepthUpdate;
 use eyre::Result;
-use tokio::{sync::mpsc, sync::mpsc::error::TryRecvError};
+use tokio::sync::mpsc;
+use updates_processor::UpdateProcessor;
 use updates_provider::UpdatesProvider;
-
-async fn get_snapshot(symbol: &str, limit: i16) -> Result<serde_json::Value> {
-    let params = [("symbol", symbol), ("limit", &limit.to_string())];
-    let client = reqwest::Client::new();
-
-    let response: serde_json::Value = client
-        .get("https://api.binance.com/api/v3/depth")
-        .query(&params)
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    Ok(response)
-}
-
-async fn init_book(min_update_id: u64, symbol: &str, limit: i16) -> Result<BookOrder> {
-    loop {
-        let snapshot_json = get_snapshot(symbol, limit).await?;
-        if let Some(update_id) = snapshot_json.get("lastUpdateId").and_then(|u| u.as_u64()) {
-            if update_id >= min_update_id {
-                return BookOrder::new(&snapshot_json);
-            }
-        }
-    }
-}
-
-fn handle_update(book: &mut BookOrder, update: &DepthUpdate) {
-    if let Err(err) = book.depth_update(&update.value) {
-        if let Some(depth_err) = err.downcast_ref::<DepthUpdateError>() {
-            match depth_err {
-                DepthUpdateError::ParsingError => {
-                    println!("Ошибка парсинга обновления книги");
-                }
-                DepthUpdateError::InvalidBook => {
-                    println!(
-                        "Ошибка: некорректная книга заявок. Книга: {} ",
-                        book.last_update_id(),
-                    );
-                }
-            }
-        } else {
-            println!("Unknown error");
-        }
-    } else {
-        println!(
-            "Update applied succefully. New id {}",
-            book.last_update_id()
-        );
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let (tx, mut rx) = mpsc::channel::<DepthUpdate>(1024);
+    let (tx, rx) = mpsc::channel::<DepthUpdate>(1024);
     let updates_receiver = tokio::spawn(async move {
         let mut depth_updates_provider = UpdatesProvider::new("BTCUSDT", 3, tx);
         depth_updates_provider.run().await
     });
 
-    let mut first_update_opt: Option<DepthUpdate> = None;
-    while let Some(update) = rx.recv().await {
-        first_update_opt = Some(update);
-        break;
-    }
-
-    if first_update_opt.is_none() {
-        return Err(eyre::eyre!("Failed to get initial update id"));
-    }
-
-    let first_update = first_update_opt.take().unwrap();
-    let mut book = init_book(first_update.first_update_id, "BTCUSDT", 10).await?;
-    book.depth_update(&first_update.value)?;
-    println!("Initial book state:\n{}", book);
-
-    tokio::spawn(async move {
-        while let Some(update) = rx.recv().await {
-            let mut latest_update = update;
-            loop {
-                match rx.try_recv() {
-                    Ok(update) => {
-                        if update.first_update_id > book.last_update_id() + 1 {
-                            handle_update(&mut book, &latest_update);
-                            latest_update = update;
-                        } else if update.last_update_id > latest_update.last_update_id {
-                            latest_update = update;
-                        }
-                    }
-                    Err(TryRecvError::Empty) => {
-                        handle_update(&mut book, &latest_update);
-                        break;
-                    }
-                    Err(TryRecvError::Disconnected) => {
-                        break;
-                    }
-                }
+    let updates_processor = tokio::spawn(async move {
+        let mut depth_updates_processor = UpdateProcessor::new("BTCUSDT", rx);
+        loop {
+            if let Ok(e) = depth_updates_processor.run(100).await {
+                println!("Invalid book state. Reiniting...");
+            } else {
+                break;
             }
         }
     });
@@ -111,5 +31,6 @@ async fn main() -> Result<()> {
     if let Err(result) = updates_receiver.await? {
         println!("Error in updates provider {:#?}", result);
     }
+    updates_processor.await?;
     Ok(())
 }
--- src/updates_processor.rs
+++ src/updates_processor.rs
@@ -1,6 +1,111 @@
-use crate::book_order::BookOrder;
+use crate::book_order::{BookOrder, DepthUpdateError};
 use crate::depth_updates::DepthUpdate;
+use eyre::Result;
+use tokio::sync::mpsc::{Receiver, error::TryRecvError};
 
 pub struct UpdateProcessor {
-    book: Option<BookOrder>,
+    symbol: String,
+    rx: Receiver<DepthUpdate>,
+    // TODO: smart pointer for book
+    // TOOD: printing job
+}
+
+impl UpdateProcessor {
+    pub fn new(symbol: &str, rx: Receiver<DepthUpdate>) -> Self {
+        UpdateProcessor {
+            symbol: symbol.to_string(),
+            rx,
+        }
+    }
+
+    pub async fn run(&mut self, snapshot_limit: usize) -> Result<()> {
+        let first_update = self
+            .rx
+            .recv()
+            .await
+            .ok_or_else(|| eyre::eyre!("Failed to get initial update id"))?;
+
+        let mut book =
+            Self::init_book(first_update.first_update_id, &self.symbol, snapshot_limit).await?;
+
+        book.depth_update(&first_update.value)?;
+        println!("Initial book state:\n{}", book);
+
+        self.reader_cycle(book).await
+    }
+
+    async fn reader_cycle(&mut self, mut book: BookOrder) -> Result<()> {
+        while let Some(update) = self.rx.recv().await {
+            let mut latest_update = update;
+            loop {
+                match self.rx.try_recv() {
+                    Ok(update) => {
+                        if update.first_update_id > book.last_update_id() + 1 {
+                            Self::handle_update(&mut book, &latest_update)?;
+                            latest_update = update;
+                        } else if update.last_update_id > latest_update.last_update_id {
+                            latest_update = update;
+                        }
+                    }
+                    Err(TryRecvError::Empty) => {
+                        Self::handle_update(&mut book, &latest_update)?;
+                        break;
+                    }
+                    Err(TryRecvError::Disconnected) => {
+                        break;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_update(book: &mut BookOrder, update: &DepthUpdate) -> Result<()> {
+        if let Err(err) = book.depth_update(&update.value) {
+            if let Some(depth_err) = err.downcast_ref::<DepthUpdateError>() {
+                match depth_err {
+                    DepthUpdateError::ParsingError => {
+                        println!("Depth update parsing error");
+                    }
+                    DepthUpdateError::InvalidBook => {
+                        return Err(eyre::eyre!("Invalid book state"));
+                    }
+                }
+            }
+        } else {
+            println!(
+                "Update applied succefully. New id {}",
+                book.last_update_id()
+            );
+        }
+
+        Ok(())
+    }
+
+    async fn init_book(min_update_id: u64, symbol: &str, limit: usize) -> Result<BookOrder> {
+        loop {
+            let snapshot_json = Self::get_snapshot(symbol, limit).await?;
+            if let Some(update_id) = snapshot_json.get("lastUpdateId").and_then(|u| u.as_u64()) {
+                if update_id >= min_update_id {
+                    return BookOrder::new(&snapshot_json);
+                }
+            }
+        }
+    }
+
+    async fn get_snapshot(symbol: &str, limit: usize) -> Result<serde_json::Value> {
+        let params = [("symbol", symbol), ("limit", &limit.to_string())];
+        let client = reqwest::Client::new();
+
+        let response: serde_json::Value = client
+            .get("https://api.binance.com/api/v3/depth")
+            .query(&params)
+            .send()
+            .await?
+            .json()
+            .await?;
+
+        Ok(response)
+    }
 }
